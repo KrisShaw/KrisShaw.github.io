d8e68dbc882d7bcdaa52203e4ead0503134ddcd0
KrisShaw <kristofershaw@gmail.com>
2021-02-07 23:59:30 +0000

    Update index.js
    trying out for loop

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var node = document.createElement("LI");                 // Create a <li> node
-var textnode = document.createTextNode("Water");         // Create a text node
-node.appendChild(textnode);                              // Append the text to <li>
-document.getElementById("hocho").appendChild(node); 
+var l = ['water', 'fire', 'ice'];
+for(let i = 0; i < l.length; i++){
+  var node = document.createElement("LI");                 // Create a <li> node
+  var textnode = document.createTextNode(l[i]);           // Create a text node
+  node.appendChild(textnode);                              // Append the text to <li>
+  document.getElementById("hocho").appendChild(node);
+}  
