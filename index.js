fa6f23f9d58663e069c2917e66b39170ef01ba68
KrisShaw <kristofershaw@gmail.com>
2021-02-07 23:25:16 +0000

    Update index.js
    testing .js via inserting <p>

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var btn = document.createElement("BUTTON");   // Create a <button> element
-btn.innerHTML = "CLICK ME";                   // Insert text
-document.body.appendChild(btn);               // Append <button> to <body>
+var btn = document.createElement("p");   // Create a <button> element
+btn.innerHTML = "This is from the index.js file, so seeing this means that it works";                   // Insert text
+document.getElementsByClassName("blurb").appendChild(btn);               // Append <button> to <body>
